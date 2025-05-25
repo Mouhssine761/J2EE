db637afe3465ad82a30f051396429e2fa78970fa
MouhssineKhchichine <Mouhssine.Khchichine@emsi-edu.ma>
2025-05-25 22:31:33 +0100

    3rd commit

---
+++ Frontend/src/app/app.module.ts
@@ -0,0 +1,9 @@
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import {NgModule} from '@angular/core';
+
+@NgModule({
+  imports: [
+    MatSnackBarModule,
+  ],
+})
+export class AppModule {}
